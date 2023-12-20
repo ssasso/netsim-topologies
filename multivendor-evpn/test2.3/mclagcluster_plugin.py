37c9fcaaaa3a5415a7becec39e1da0bb742d59f7
Stefano Sasso <852093+ssasso@users.noreply.github.com>
2023-12-20 16:22:47 +0000

    multivendor-evpn: test2.3 initial

--- multivendor-evpn/test2.3/mclagcluster_plugin.py
+++ multivendor-evpn/test2.3/mclagcluster_plugin.py
@@ -13,6 +13,16 @@ def init(topology: Box) -> None:
     topology.defaults.attributes.node.mclagcluster = None
     return
 
+def transform_vtep_list(original, vteps_replacement, myself = ''):
+    vteps = []
+    for vt in original:
+        if vt in vteps_replacement:
+            repl = vteps_replacement[vt]
+            if repl != myself:
+                vteps.append(vteps_replacement[vt])
+        else:
+            vteps.append(vt)
+    return list(dict.fromkeys(vteps))
 
 def post_transform(topology: Box) -> None:
     print('MCLAG PLUGIN POST_TRANSFORM')
@@ -45,23 +55,16 @@ def post_transform(topology: Box) -> None:
                     topology.nodes[n]['anycast_vtep'] = {
                             'loopback': ifindex_offset,
                             'address': str(vtep_a),
+                            'plain_addr': str(vtep_a.network),
                             }
     print('VTEP REPLCS')
     print(vteps_replacement)
     for n, ndata in topology.nodes.items():
-        print('XX Node {}'.format(n))
+        myself = ''
+        if 'anycast_vtep' in ndata:
+            myself = ndata.anycast_vtep.plain_addr
         if 'vxlan' in ndata and ndata.vxlan.get('flooding', '') == 'static':
-            node_vteps = []
-            for vt in ndata.vxlan.get('vtep_list', []):
-                print('    vt {}'.format(vt))
-                if vt in vteps_replacement:
-                    print('     found in repl list')
-                    node_vteps.append(vteps_replacement[vt])
-                else:
-                    node_vteps.append(vt)
-            # remove duplicates
-            ndata.vxlan.vtep_list = list(dict.fromkeys(node_vteps))
-            ##### TODO: remove vtep of current cluster
+            ndata.vxlan.vtep_list = transform_vtep_list(ndata.vxlan.get('vtep_list', []), vteps_replacement, myself)
             ##### TODO: VTEP FLOOD LIST IS PER VNI!!!
     return
 
