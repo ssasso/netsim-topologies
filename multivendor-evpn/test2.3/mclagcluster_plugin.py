bdb9a542a145a22ee80ea6dfd5054c8fd791dce5
Stefano Sasso <852093+ssasso@users.noreply.github.com>
2023-12-21 13:36:08 +0000

    multivendor-evpn: test2.3 limit scope

--- multivendor-evpn/test2.3/mclagcluster_plugin.py
+++ multivendor-evpn/test2.3/mclagcluster_plugin.py
@@ -13,20 +13,8 @@ def init(topology: Box) -> None:
     topology.defaults.attributes.node.mclagcluster = None
     return
 
-def transform_vtep_list(original, vteps_replacement, myself = ''):
-    vteps = []
-    for vt in original:
-        if vt in vteps_replacement:
-            repl = vteps_replacement[vt]
-            if repl != myself:
-                vteps.append(vteps_replacement[vt])
-        else:
-            vteps.append(vt)
-    return list(dict.fromkeys(vteps))
-
 def post_transform(topology: Box) -> None:
     print('MCLAG PLUGIN POST_TRANSFORM')
-    vteps_replacement = {}
     for g,gdata in topology.get('groups', {}).items():
         mclagdata = gdata.get('node_data', {}).get('mclagcluster', {})
         if 'transit_vlan' in mclagdata and len(gdata.get('members', [])) == 2:
@@ -43,28 +31,5 @@ def post_transform(topology: Box) -> None:
                 print('      N: {} IP: {}'.format(n, ip_address))
                 addr_id = addr_id + 1
                 topology.nodes[n]['mclagcluster']['transit_ip'] = ip_address
-            # Consider also VXLAN ANYCAST VTEP
-            if 'vxlan' in topology:
-                # vtep address
-                vtep_a = addressing.get(topology.pools, ['anycast_vtep', 'loopback'])['ipv4']
-                print('    VTEP: {}'.format(vtep_a))
-                for n in gdata.get('members', []):
-                    if 'vtep' in topology.nodes[n].vxlan:
-                        vteps_replacement[topology.nodes[n].vxlan.vtep] = str(vtep_a.network)
-                    ifindex_offset = ( devices.get_device_attribute(topology.nodes[n], 'loopback_offset', topology.defaults) or 1 )
-                    topology.nodes[n]['anycast_vtep'] = {
-                            'loopback': ifindex_offset,
-                            'address': str(vtep_a),
-                            'plain_addr': str(vtep_a.network),
-                            }
-    for n, ndata in topology.nodes.items():
-        myself = ''
-        if 'anycast_vtep' in ndata:
-            myself = ndata.anycast_vtep.plain_addr
-        if 'vxlan' in ndata and ndata.vxlan.get('flooding', '') == 'static':
-            ndata.vxlan.vtep_list = transform_vtep_list(ndata.vxlan.get('vtep_list', []), vteps_replacement, myself)
-            for vl, vdata in ndata.get('vlans', {}).items():
-                if 'vtep_list' in vdata:
-                    vdata.vtep_list = transform_vtep_list(vdata.vtep_list, vteps_replacement, myself)
     return
 
